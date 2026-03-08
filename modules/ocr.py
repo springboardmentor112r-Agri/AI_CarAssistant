69306404c07ac585e42787d4823b7bfdbaf5ced5
rozuoo <rosesoyuse@gmail.com>
2026-03-09 01:31:20 +0530

    Refactor OCR text extraction for consistency

--- modules/ocr.py
+++ modules/ocr.py
@@ -8,29 +8,26 @@ def extract_text(file):
 
     text = ""
 
-    # If file is a PDF
     if file.type == "application/pdf":
 
-        # Try extracting selectable text first
         with pdfplumber.open(file) as pdf:
             for page in pdf.pages:
                 extracted = page.extract_text()
                 if extracted:
                     text += extracted + "\n"
 
-        # If no text found → use OCR
         if text.strip() == "":
             file.seek(0)
             images = convert_from_bytes(file.read())
 
             for img in images:
-                img = img.convert("L")  # convert to grayscale
+                img = img.convert("L")
                 text += pytesseract.image_to_string(img, config="--psm 6")
 
-    # If file is an image
     else:
+
         image = Image.open(file)
-        image = image.convert("L")  # grayscale for better OCR
+        image = image.convert("L")
         text = pytesseract.image_to_string(image, config="--psm 6")
 
     return text
